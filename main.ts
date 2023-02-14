87dca085b9f555678505598a3fd14ccfd2a9a88d
ro0k23 <125355361+ro0k23@users.noreply.github.com>
2023-02-14 10:40:43 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let dado = 0
+input.onGesture(Gesture.Shake, function () {
+    if (6 == dado) {
+        dado = randint(1, 6)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else if (5 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (4 == dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (3 == dado) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (2 == dado) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (1 == dado) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
